1a7b49d11ef2f5f60edbfdc4449bf414167fcae1
Joao Patacas <njp232@newcastle.ac.uk>
2026-03-13 12:24:15 +0000

    initial implementation of DT modes in sidebar tools

--- src/components/building/side-menu/sidebar-tools.tsx
+++ src/components/building/side-menu/sidebar-tools.tsx
@@ -4,50 +4,66 @@ import { type Tool } from "../../../types";
 import { type FrontMenuMode } from "../types";
 import ListIcon from "@mui/icons-material/ViewList";
 import MapIcon from "@mui/icons-material/Map";
-import DeleteIcon from '@mui/icons-material/Delete';
-import LogoutIcon from '@mui/icons-material/Logout';
-import ModelIcon from '@mui/icons-material/HolidayVillage';
+import DeleteIcon from "@mui/icons-material/Delete";
+import LogoutIcon from "@mui/icons-material/Logout";
+import ModelIcon from "@mui/icons-material/HolidayVillage";
+import AnalyticsIcon from "@mui/icons-material/Insights";
 
 export function getSidebarTools(
   state: State,
   dispatch: React.Dispatch<Action>,
   toggleMenu: (active: boolean, mode?: FrontMenuMode) => void,
 ): Tool[] {
-  return [
+  const tools: Tool[] = [];
+
+  tools.push(
     {
       name: "Info",
       icon: <ListIcon />,
-      action: () => {
-        toggleMenu(true, "BuildingInfo"); //true, "mode of the menu"
-      }
+      action: () => toggleMenu(true, "BuildingInfo"),
     },
     {
       name: "Model list",
       icon: <ModelIcon />,
-      action: () => {
-        toggleMenu(true, "ModelList"); //true, "mode of the menu"
-      },
+      action: () => toggleMenu(true, "ModelList"),
     },
+  );
+
+  if (state.dtMode === "diagnostic") {
+    tools.push({
+      name: "Diagnostics",
+      icon: <AnalyticsIcon />,
+      action: () => console.log("diagnostics"),
+    });
+  }
+
+  if (state.dtMode === "performance") {
+    tools.push({
+      name: "Performance",
+      icon: <AnalyticsIcon />,
+      action: () => console.log("performance"),
+    });
+  }
+
+  tools.push(
     {
       name: "Back to map",
       icon: <MapIcon />,
-      action: () => {
-        dispatch({ type: "CLOSE_BUILDING" });
-      },
+      action: () => dispatch({ type: "CLOSE_BUILDING" }),
     },
     {
       name: "Logout",
       icon: <LogoutIcon />,
-      action: () => {
-        dispatch({ type: "LOGOUT" });
-      },
+      action: () => dispatch({ type: "LOGOUT" }),
     },
-    {
-      name: "Delete building",
-      icon: <DeleteIcon />,
-      action: () => {
-        dispatch({ type: "DELETE_BUILDING", payload: state.building });
-      },
-    },
-  ];
+  );
+
+  tools.push({
+    name: "Delete building",
+    icon: <DeleteIcon />,
+    action: () =>
+      dispatch({ type: "DELETE_BUILDING", payload: state.building }),
+  });
+
+  return tools;
 }
--- src/components/map/side-menu/map-tools.tsx
+++ src/components/map/side-menu/map-tools.tsx
@@ -1,30 +1,54 @@
-import { type Action } from "../../../middleware/actions";
-import { type State } from "../../../middleware/state";
-import { type Tool } from "../../../types";
+import type { State } from "../../../middleware/state";
+import type { Action } from "../../../middleware/actions";
+import type { Tool } from "../../../types";
+
 import AddBuildingIcon from "@mui/icons-material/DomainAdd";
 import LogoutIcon from "@mui/icons-material/Logout";
+import AnalyticsIcon from "@mui/icons-material/Insights";
 
 export function getMapTools(
   state: State,
   dispatch: React.Dispatch<Action>,
-  toggleMenu: (active: boolean) => void,
-  toggleCreate: () => void
+  toggleCreate: () => void,
 ): Tool[] {
-  return [
-    {
-      name: "Create Building",
-      icon: <AddBuildingIcon />,
-      action: () => {
-        toggleCreate();
-        toggleMenu(false);
-      },
-    },
-    {
-      name: "Logout",
-      icon: <LogoutIcon />,
-      action: () => {
-        dispatch({ type: "LOGOUT" });
-      },
-    },
-  ];
+  const mode = state.dtMode;
+
+  const tools: Tool[] = [];
+
+  tools.push({
+    name: "Create building",
+    icon: <AddBuildingIcon />,
+    action: toggleCreate,
+  });
+
+  // Descriptive
+  if (mode === "descriptive") {
+
+  }
+
+  // Diagnostic
+  if (mode === "diagnostic") {
+    tools.push({
+      name: "District diagnostics",
+      icon: <AnalyticsIcon />,
+      action: () => console.log("diagnostics"),
+    });
+  }
+
+  // Performance
+  if (mode === "performance") {
+    tools.push({
+      name: "District performance",
+      icon: <AnalyticsIcon />,
+      action: () => console.log("performance"),
+    });
+  }
+
+  tools.push({
+    name: "Logout",
+    icon: <LogoutIcon />,
+    action: () => dispatch({ type: "LOGOUT" }),
+  });
+
+  return tools;
 }
