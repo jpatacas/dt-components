84761989295fcc599f28c23bedecda375876bca4
Joao Patacas <njp232@newcastle.ac.uk>
2026-06-22 16:47:22 +0100

    implemented sensor graph visualisation in properties menu using MUI X

--- src/components/building/front-menu/front-menu-content/properties-menu.tsx
+++ src/components/building/front-menu/front-menu-content/properties-menu.tsx
@@ -1,10 +1,19 @@
 import { Divider } from "@mui/material";
+import { LineChart } from "@mui/x-charts/LineChart";
 import { type FC } from "react";
 import { useAppContext } from "../../../../middleware/context-provider";
 import "./front-menu-content.css";
 
 export const PropertiesMenu: FC = () => {
-  const [state] = useAppContext();
+  const [state, dispatch] = useAppContext();
+
+  const chartData =
+    state.sensorHistory
+      ?.filter((_, index) => index % 10 === 0)
+      .map((point) => ({
+        time: new Date(point.time),
+        value: Number(point.value),
+      })) ?? [];
 
   return (
     <div>
@@ -14,16 +23,70 @@ export const PropertiesMenu: FC = () => {
         <p>No item selected.</p>
       )}
 
-      {state.properties.map((property) => (
-        <div key={property.name}>
-          <div className="value-pair list-item">
-            <div>{property.name}</div>
-            <p>:</p>
-            <div>{property.value}</div>
+      {state.properties.map((property) => {
+        const isSensor = property.type === "sensor";
+
+        return (
+          <div key={property.name}>
+            <div
+              className={`value-pair list-item ${isSensor ? "sensor-row" : ""}`}
+              onClick={() => {
+                if (!isSensor) return;
+
+                dispatch({
+                  type: "SELECT_SENSOR",
+                  payload: {
+                    name: property.name,
+                    timeseriesId: property.timeseriesId,
+                    unit: property.unit,
+                  },
+                });
+              }}
+            >
+              <div>{property.name}</div>
+              <p>:</p>
+              <div>{property.value}</div>
+            </div>
+
+            <Divider />
           </div>
+        );
+      })}
+
+      {state.selectedSensor && state.sensorHistory.length > 0 && (
+        <>
           <Divider />
-        </div>
-      ))}
+
+          <div style={{ padding: "1rem" }}>
+            <h4>
+              {state.selectedSensor.name}
+              {state.selectedSensor.unit && ` (${state.selectedSensor.unit})`}
+            </h4>
+
+            <LineChart
+              height={300}
+              dataset={chartData}
+              xAxis={[
+                {
+                  dataKey: "time",
+                  scaleType: "time",
+                  valueFormatter: (value) =>
+                    new Date(value).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    }),
+                },
+              ]}
+              series={[
+                {
+                  dataKey: "value",
+                  label: state.selectedSensor.name,
+                },
+              ]}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };
